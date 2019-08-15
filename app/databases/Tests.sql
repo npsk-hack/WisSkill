2df46b53ad862a717632ade052fa2618c81f3ea5
vibes23 <54066792+vibes23@users.noreply.github.com>
2019-08-16 00:40:25 +0530

    Add files via upload

---
+++ app/databases/Tests.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `TestData` (
+  `test_no` INT NOT NULL,
+  `question_no` INT NOT NULL,
+  `question` VARCHAR(1000) NULL,
+  `option_a` VARCHAR(200) NULL,
+  `option_b` VARCHAR(200) NULL,
+  `option_c` VARCHAR(200) NULL,
+  `option_d` VARCHAR(200) NULL,
+  `correct_option` CHAR(1) NULL,
+  PRIMARY KEY (`test_no`, `question_no`))
+
+CREATE TABLE `Tests` (
+  `test_no` INT NOT NULL,
+  `teacher_id` INT NOT NULL,
+  PRIMARY KEY (`test_no`))
