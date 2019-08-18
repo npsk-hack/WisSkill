d0916395a604313ec7fb416f6fe53f606a98e073
vibes23 <54066792+vibes23@users.noreply.github.com>
2019-08-18 23:42:31 +0530

    Update Tests.sql

--- app/databases/Tests.sql
+++ app/databases/Tests.sql
@@ -2,9 +2,9 @@ CREATE TABLE `TestData` (
   `test_no` INT NOT NULL,
   `question_no` INT NOT NULL,
   `question` VARCHAR(1000) NULL,
-  `option_a` VARCHAR(200) NULL,
-  `option_b` VARCHAR(200) NULL,
-  `option_c` VARCHAR(200) NULL,
-  `option_d` VARCHAR(200) NULL,
+  `option_a` VARCHAR(85) NULL,
+  `option_b` VARCHAR(85) NULL,
+  `option_c` VARCHAR(85) NULL,
+  `option_d` VARCHAR(85) NULL,
   `correct_option` CHAR(1) NULL,
   PRIMARY KEY (`test_no`, `question_no`))
