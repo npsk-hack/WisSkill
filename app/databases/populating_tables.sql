6ce492002a50e96fada47b4e0d10713979f0f959
unknown <anirudh.mss@gmail.com>
2019-08-19 22:29:36 +0530

    additional changes

--- app/databases/populating_tables.sql
+++ app/databases/populating_tables.sql
@@ -6,9 +6,9 @@ VALUES ('Samarth M', 'samarthm', 8, 'D', 60015, 'Math', 'Indian Music'),
 	   ('Aditya K', 'adityak', 10, 'A', 52912, 'Heritage', 'Western Music'),
        ('Raaghav O', 'raaghavo', 10, 'C', 59991, 'Computer Science', 'Art');
 INSERT INTO teachers
-VALUES ('Malathy R Narayan','malathirn','false','{"11A" : "Biology", "12B" : "Biology", "10C" : "Chemistry"}', 001),
-	   ('Veena L','veenal','6B','{"6B" : "English", "6A" : "English", "7D" : "English", "8C" : "English", "2D" : "Life Skills"}', 093),
-       ('Poornima C','poornimac','false','{"all":"Yoga"}', 143);
+VALUES ('Malathy R Narayan','malathirn','false', 001),
+	   ('Veena L','veenal','6B', 093),
+       ('Poornima C','poornimac','false', 143);
 #{all:yoga} means she teaches to all classes
 #{8:yoga} means she teaches to all 8th grade classes
 INSERT INTO admins
@@ -16,6 +16,19 @@ VALUES (0, 'anirudh', 'creator', 'Anirudh', 1),
 	   (1, 'vaibhav', 'creator', 'Vaibhav', 1),
        (2, 'archith', 'creator', 'Archith', 1),
 	   (3, 'npsrnr', 'main_admin', 'Malathy R Narayan', 2);
+
+INSERT INTO subjects (`subject`, grade, section, teacher_id)
+VALUES ('Biology', 11, 'A', 001),
+	   ('Biology', 12, 'A', 001),
+       ('Biology', 11, 'B', 001),
+       ('English', 7, 'C', 093),
+       ('English', 8, 'D', 093),
+       ('English', 6, 'B', 093),
+       ('Yoga', 5, 'D', 143),
+       ('Yoga', 4, 'C', 143),
+       ('Yoga', 3, 'A', 143),
+       ('Yoga', 10, 'A', 143);
+
 SELECT * FROM students;
 SELECT * FROM teachers;
 SELECT * FROM admins;
--- app/databases/setup.sql
+++ app/databases/setup.sql
@@ -9,13 +9,12 @@ CREATE TABLE students(
     admission_no INTEGER NOT NULL PRIMARY KEY,
     elective_club VARCHAR(255),
     elective_vpa VARCHAR(255)
-);
+)  ENGINE = INNODB;
 
 CREATE TABLE teachers(
 	name_of_teacher VARCHAR(255) NOT NULL,
     username_of_teacher VARCHAR(255) NOT NULL,
     class_teacher VARCHAR(5),
-    classes_with_subject  TEXT,
     teacher_id INTEGER NOT NULL PRIMARY KEY
 );
 
@@ -26,3 +25,25 @@ CREATE TABLE admins(
     name_of_admin VARCHAR(255) NOT NULL,
     level_of_access INTEGER NOT NULL
 );
+
+CREATE TABLE subjects(
+	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `subject` VARCHAR(20) NOT NULL,
+    grade INTEGER(2) NOT NULL,
+    section VARCHAR(1) NOT NULL,
+    teacher_id INTEGER NOT NULL,
+    FOREIGN KEY (teacher_id) REFERENCES teachers(teacher_id)
+) ENGINE = INNODB;
+
+CREATE TABLE doubts(
+	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `subject` VARCHAR(100) NOT NULL,
+    question TEXT NOT NULL,
+    student INTEGER NOT NULL,
+    teacher INTEGER NOT NULL,
+    answer TEXT,
+    FOREIGN KEY (student) REFERENCES students(admission_no),
+    FOREIGN KEY (teacher) REFERENCES teachers(teacher_id)
+) ENGINE = INNODB;
+
+# 18:48:21	CREATE TABLE subjects(  id INTEGER NOT NULL PRIMARY KEY,     `subject` VARCHAR(20) NOT NULL,     grade INTEGER(2) NOT NULL,     section VARCHAR(1) NOT NULL,     teacher VARCHAR(100) NOT NULL,     FOREIGN KEY (teacher) REFERENCES teachers(teacher_id) )	Error Code: 1215. Cannot add foreign key constraint	0.953 sec
